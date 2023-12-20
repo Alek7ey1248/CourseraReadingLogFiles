e57ceb88c1a454dbd93104b5a7493d81d2569811
Alek7ey1248 <alek7ey1248@gmail.com>
2023-12-20 16:05:44 -0500

    почти все сделал в след практике, осталось один метод

--- src/LogAnalyzer.java
+++ src/LogAnalyzer.java
@@ -82,5 +82,92 @@ public class LogAnalyzer
          }
          return uniqueIPVisits;
     }
-     
+
+
+
+    // метод возвращает количество уникальных IP-адресов в записях,
+    // которые имеют код состояния statusCode в диапазоне от low до high, включительно
+    public  int countUniqueIPsInRange(int low, int high) {
+         int i = 0;
+         ArrayList<String> uniqueIPsInRange = new ArrayList<String>();
+         for (LogEntry le : records) {
+             if (le.getStatusCode() >= low && le.getStatusCode() <= high) {
+                 if (!uniqueIPsInRange.contains(le.getIpAddress())) {
+                     uniqueIPsInRange.add(le.getIpAddress());
+                     i++;
+                     System.out.println(i + "\t" + le.getIpAddress());
+                 }
+             }
+         }
+         return uniqueIPsInRange.size();
+    }
+
+
+    // метод возвращает HashMap<String, Integer>, который сопоставляет
+    // IP-адрес с количеством появлений этого IP-адреса в записях,
+    // что означает количество посещений сайта с этого IP-адреса
+    public HashMap<String, Integer> countVisitsPerIP() {
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for (LogEntry le : records) {
+            String ip = le.getIpAddress();
+            if (!map.containsKey(ip)) {
+                map.put(ip, 1);   // если в map еще нет IP - добавляем ключ IP c значением 1
+            } else {
+                map.put(ip, map.get(ip) + 1);   // если уже есть в map такой IP, то увеличиваем его значение на 1
+            }
+        }
+        return map;
+    }
+
+
+
+    // метод возвращает максимальное количество посещений данного сайта одним IP-адресом.
+    // HashMap<String, Integer> map берется из метода countVisitsPerIP()
+    public int mostNumberVisitsByIP(HashMap<String, Integer> map) {
+         int max = 0;
+         for (Integer value : map.values()) {
+             if (value > max) max = value;
+         }
+         return  max;
+    }
+
+
+
+    // метод возвращает ArrayList строк IP-адресов, каждый из которых
+    // имеет максимальное количество посещений данного сайта.
+    // HashMap<String, Integer> map берется из метода countVisitsPerIP()
+    public ArrayList<String> iPsMostVisits(HashMap<String, Integer> map) {
+        ArrayList<String> ipAddresses = new ArrayList<String>();
+         // методом, описанным выше найдем максимальное значение
+         int maxValue = mostNumberVisitsByIP(map);
+        // теперь запишем в ArrayList все ключи(IP адреса) которые соответствуют максимальному значению
+        for (String key : map.keySet()) {
+            if (maxValue == map.get(key)) ipAddresses.add(key);
+        }
+        return ipAddresses;
+    }
+
+
+
+    // метод возвращает HashMap<String, ArrayList<String>>,
+    // с ArrayList'ом IP-адресов, которые встречались в конкретную дату
+    // (включая повторяющиеся IP-адреса)
+    public HashMap<String, ArrayList<String>> iPsForDays() {
+        HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        for (LogEntry le : records) {
+            String data = le.getAccessTime().toString();
+            data = data.substring(3, 10);      // вырезаем только дату
+            String ip = le.getIpAddress();
+            if (!map.containsKey(data)) {
+                ArrayList<String> newList = new ArrayList<String>();
+                newList.add(ip);
+                map.put(data, newList);
+            } else {
+                ArrayList<String> list = map.get(data);
+                list.add(ip);
+                map.put(data, list);
+            }
+        }
+        return map;
+    }
 }
--- src/Tester.java
+++ src/Tester.java
@@ -20,6 +20,8 @@ public class Tester
     public void testLogAnalyzer() {
         LogAnalyzer la = new LogAnalyzer();
         String fileLogName = "weblog-short_log";
+        //String fileLogName = "weblog3-short_log";
+        //String fileLogName = "weblog1_log";
 
         System.out.println(" загружаем построчно fileLogName - " + fileLogName + " в ArrayList<LogEntry> records");
         la.readFile(fileLogName);
@@ -31,13 +33,49 @@ public class Tester
         System.out.println(" Кол-во уникальных IP адресов в журнале - " + countUniqIPs);
         System.out.println();
 
-        int num = 200 ;
-        System.out.println(" LogEntrys (записи журнала), которые имеют код состояния больше, чем num");
-        la.printAllHigherThanNum(200);
+        int num = 400 ;
+        System.out.println(" LogEntrys (записи журнала), которые имеют код состояния больше, чем " + num);
+        la.printAllHigherThanNum(num);
         System.out.println();
 
-        String data = "Sep 14";
-        System.out.println(" ArrayList -  строк уникальных IP-адресов, которые имели доступ в данный день");
+        String data = "Mar 24";
+        System.out.println(" ArrayList -  строк уникальных IP-адресов, которые имели доступ " + data);
         ArrayList<String> uniqIPVisitsOnDay = la.uniqueIPVisitsOnDay(data);
+        System.out.println();
+
+        int low = 300;
+        int high = 399;
+        System.out.println(" уникальные IP адреса записей журнала, которые имеют код состояния statusCode в диапазоне от " + low + " до " +  high + ", включительно");
+        int countUniqueIPsInRange = la.countUniqueIPsInRange(low, high);
+        System.out.println(" Их кол-во - " + countUniqueIPsInRange);
+        System.out.println();
+
+        System.out.println("--------------------------------------" );
+        System.out.println("--------------------------------------" );
+        System.out.println("HashMap, в котором КЛЮЧИ - IP-адреса, а ЗНАЧЕНИЯ - количество посещений сайта с этого IP-адреса ");
+        HashMap<String, Integer> countVisits = la.countVisitsPerIP();
+        int i = 0;
+        for (Map.Entry<String, Integer> map : countVisits.entrySet()) {
+            i++;
+            System.out.println(i + "\t" + map.getKey() + " - " + "\t" + map.getValue());
+        }
+        System.out.println();
+
+        int maxValue = la.mostNumberVisitsByIP(countVisits);
+        System.out.println(" максимальное количество посещений данного сайта одним IP-адресоv - " + maxValue);
+        System.out.println();
+
+        ArrayList<String> ipAdressesMaxValue = la.iPsMostVisits(countVisits);
+        System.out.println(" ArrayList<String> строк IP-адресов, каждый из которых имеет максимальное количество посещений данного сайта");
+        System.out.println(ipAdressesMaxValue.toString());
+        System.out.println();
+
+        HashMap<String, ArrayList<String>> mapIPsForDays = la.iPsForDays();
+        System.out.println(" HashMap<String, ArrayList<String>>, с ArrayList'ом IP-адресов, которые встречались в конкретную дату (включая повторяющиеся IP-адреса)");
+        int n = 0;
+        for (String dataKey : mapIPsForDays.keySet()) {    // по ключам
+            n++;
+            System.out.println(n + "\t" + dataKey + " - " + "\t" + mapIPsForDays.get(dataKey).toString());
+        }
     }
 }
